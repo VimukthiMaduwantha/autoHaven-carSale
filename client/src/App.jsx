f65ae677362d4968a47d6483dcc2c497dbf0d2f7
IT21031120 <it21031120@my.sliit.lk>
2023-08-28 20:21:40 +0530

    init commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import NavBarView from './components/NavBarView'
+
+function App() {
+  return (
+    <>
+      <NavBarView />
+
+    </>
+  )
+}
+
+export default App
